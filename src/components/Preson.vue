7d5d916d55e5ab6e605a4f641b23b7db391d9a61
fanxiaofeng <fanxiaofeng@qianlima.com>
2024-03-04 15:10:32 +0800

    初始化

--- src/components/Preson.vue
+++ src/components/Preson.vue
@@ -1,50 +1,69 @@
 <template>
     <div class="person">
-        <h2>一辆{{ car.brand }}车,价值{{ car.price }}</h2>
-        <button @click="changePrice">改价</button>
-        <br>
-        <h2>游戏列表</h2>
-        <ul>
-            <li v-for="g in games" :key="g.id">{{g.name}}</li>
-        </ul>
+        <h2>name:{{ person.name }}</h2>
+        <h2>age:{{ person.age }}</h2>
+        <h2>汽车:{{ person.car }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="changeC1">修改第一台车</button>
+        <button @click="changeC2">修改第二台车</button>
+        <button @click="changeCar">修改整个车</button>
     </div>
-
 </template>
 
 
 
 
-<script setup lang="ts" name = "Person234">
-    import {ref} from 'vue'
-    import { reactive } from 'vue';
+<script setup lang="ts" name = "Person">
+import { reactive, ref, watch } from 'vue'
 
-    let car =reactive({brand:'奔驰',price:100})
-    let games = [
-        {id:'01',name:'王者'},
-        {id:'02',name:'原神'},
-        {id:'03',name:'lol'}
-    ]
-
-
-    function changePrice(){
-        car.price +=100;
-
+let person = reactive({
+    name: '张三',
+    age:18,
+    car:{
+        c1:'奔驰',
+        c2:'宝马'
+    
     }
+})
+
+function changeName(){
+    person.name += '~'
+}
+function changeAge(){
+    person.age += 1
+}
+
+function changeC1(){
+    person.car.c1 += '奥迪'
+}
+
+function changeC2(){
+    person.car.c2 += '大众'
+}
+
+function changeCar(){
+    person.car ={c1:'雅迪',c2:'爱玛'}
+}
+
+
 
 
 </script>
 
 <style scoped>
- .person {
-   background-color: skyblue;
-   box-shadow: 0 0 10px;
-   border-radius: 10px;
-   padding: 20px;
- }
- button {
+.person {
+    background-color: skyblue;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
     margin: 0 5px;
- }
- li {
+}
+
+li {
     font-size: 20px;
- }
+}
 </style>
